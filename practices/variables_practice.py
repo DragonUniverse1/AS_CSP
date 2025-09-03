30eec1bffaae62afdffaafad6b7117a4e803a4e6
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-09-03 14:30:13 -0600

    added variables/print statement

---
+++ practices/variables_practice.py
@@ -0,0 +1,12 @@
+# AS 7th Variables Practice - Python
+
+print("What is your name?")
+
+Name = "Alexa"
+age = 14
+
+small_num = 7
+big_num = 99999999999
+fav_sub = input("what is your favorite subject in school? ")
+
+print(fav_sub)
