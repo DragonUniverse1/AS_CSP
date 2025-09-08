826b60075da91b9e1a232aed66098a3dd3641a95
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-09-08 20:43:10 +0000

    strings

---
+++ notes/strings.py
@@ -0,0 +1,13 @@
+# AS 7th string notes
+
+# examples
+first_name = input("What is your name:\n").strip().title()
+
+last_name = input("what is your last name:\n").strip().title()
+
+full_name = first_name + " " + last_name
+
+sentence = '                 the quick brown fox jumps over the lazy dog              '
+print(sentence.strip())
+
+print("welcome to the program", full_name + "!")
