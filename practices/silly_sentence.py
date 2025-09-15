67aee44168eadc33f39eb7b2506e6242074ebd32
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-09-15 20:39:06 +0000

    function notes finished?

--- practices/silly_sentence.py
+++ practices/silly_sentence.py
@@ -1,5 +1,8 @@
 # AS 7th Silly sentences - Python
 
-name = input("what is your name:\n")
+verb = input("give me a verb:\n")
+adjective = input("give me an adjective:\n")
+verb_two = input("I need another verb:\n")
 
-print("hello", name)
+print("Today I am going to", verb ".")
+print("This is", adjective "because I really want to")
