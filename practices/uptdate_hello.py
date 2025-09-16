1849d72aa439accf8f151ee0a967476263d0c7a8
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-09-16 20:35:34 +0000

    finished(?) update hello practice

---
+++ practices/uptdate_hello.py
@@ -0,0 +1,11 @@
+# AS 7th update Hello world
+
+def hello(name):
+    print(f"Hello {name}")
+
+name = input("what is your name:\n")
+hello("Alexa")
+hello("Neven")
+hello("Jeff")
+hello("Destiny")
+hello("Rose")
