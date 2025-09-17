1e2fd734b5b817723a14d147c039bba239e4a0b5
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-09-17 20:42:21 +0000

    conditionals

---
+++ notes/conditionals.py
@@ -0,0 +1,31 @@
+# AS 7th Conditionals notes
+
+#homework_done = input("is your homework done: ").strip().capitalize()
+
+#if homework_done == "yes":
+    #print("yes, you can go")
+#else:
+    #print("go do your homework!")
+
+grade = 89
+
+if grade >= 89:
+    print(f"You have grade {grade}% that is an A!")
+elif grade >= 79:
+    print(f"you have a {grade}% that is a B!")
+elif grade >= 70:
+    print(f"you have a {grade}% that is a C!")
+else:
+    print(f"You have a {grade}% that is not an A :<")
+# if is telling the computer if it is true or false
+# boolean statement or condition is a true or false (line 5)
+# one = is a comand
+# two = is a quesion. EX: "are these the same/equal?"
+# something indented means its inside of something
+# < is greater than
+# > is less than
+# <= is less than or equal to
+# >= is greater than or equal to
+
+
+# need to write elif when have more than 2 needed outcomes
