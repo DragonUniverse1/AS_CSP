f830fa5106ac99a844d9b665e962a4030994551c
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-09-22 20:11:37 +0000

    loops

--- notes/lists.py
+++ notes/lists.py
@@ -2,3 +2,12 @@
 
 siblings = ["Dallin", "Alexa", "Chris", "Timothy", ["Destiny", "Rune", "Rose", "Eirian"]]
 # brackets[] are what makes lists; every comma separates a different word in the list
+
+print(siblings[1])
+print(siblings)
+siblings[1] = "Des"
+siblings[2] = "Child"
+siblings[3] = "Tim"
+
+siblings.remove("Dallin")
+print(siblings[1])
---
+++ notes/loops.py
@@ -0,0 +1,6 @@
+# AS 7th Loops notes
+
+foods = ["chocolate", "pasta", "Brownies", "cookies", "garlic bread", "Chocolate milk"]
+
+# For loop used for lists
+# loops repeat the code inside for a specific number of time
