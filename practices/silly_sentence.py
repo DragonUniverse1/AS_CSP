25b7c696c91d64c7e3d4ab6b5b29ded72878d77e
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-09-24 14:35:00 -0600

    game group

--- practices/silly_sentence.py
+++ practices/silly_sentence.py
@@ -4,5 +4,5 @@ verb = input("give me a verb:\n")
 adjective = input("give me an adjective:\n")
 verb_two = input("I need another verb:\n")
 
-print("Today I am going to", verb ".")
-print("This is", adjective "because I really want to")
+print("Today I am going to", verb, ".")
+print("This is", adjective, "because I really want to")
