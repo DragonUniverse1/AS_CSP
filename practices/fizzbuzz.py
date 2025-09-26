7a454e23f847a247562b31ec5b5ef7d711716513
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-09-26 03:19:52 +0000

    finished Fizz Buzz

--- practices/fizzbuzz.py
+++ practices/fizzbuzz.py
@@ -1,7 +1,14 @@
 # AS 7th FizzBuzz
 
-n = 1
+num = 1
 
-while n <= 50:
-    print(n)
-    n += 1
+while num < 51:
+    if num % 3 == 0 and num % 5 == 0:
+        print("FizzBuzz")
+    elif num % 3 == 0:
+        print("Fizz")
+    elif num % 5 == 0:
+        print("Buzz")
+    else:
+        print(num)
+    num += 1
