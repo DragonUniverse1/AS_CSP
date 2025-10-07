87d78bd54e2819f1fc2806deadd3a450ece604f4
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-10-07 02:21:52 +0000

    finished variable practice

--- c_programming/practices/variable_practice.c
+++ c_programming/practices/variable_practice.c
@@ -1,9 +1,12 @@
 // AS 7th Variable practice
 
-int main(void){
-    char name;
+int main(){
+    char name[50]; // declares character array to store name
 
-    printf("What is your name:\n");
-    scanf("%c", &name);
+    printf("What is your name:\n"); // prompt the user for input
+
+    // read the name using fgets()
+    // fgets(buffer, size, stream) reads up to size 1 characters from scteam into buffer
+    fgets(name, sizeof(name));
 
 }
