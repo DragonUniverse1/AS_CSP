a9a151c36bd87e83fdb1b00e071cab453d25556a
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-10-09 14:42:09 -0600

    must finish name decorator

--- c_programming/practices/name_decorator.c
+++ c_programming/practices/name_decorator.c
@@ -2,7 +2,15 @@
 #include <stdio.h>
 
 int main(void){
-    char name[]= "Alexa";
+    char name[25];
+    printf("tell me your name: \n");
+    scanf("%s", name);
+    printf("[%s]\n", name );
+
+    printf("[%s]\n", name);
+    strcat(name, " ");
+
+    printf("Hello -x/%s/x-");
 
     return 0;
 }
