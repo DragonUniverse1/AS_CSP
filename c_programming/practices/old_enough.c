67e906e9b9711cae28e4e039f0c84a8d7a916342
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-10-15 02:46:11 +0000

    finished old enough

--- c_programming/practices/old_enough.c
+++ c_programming/practices/old_enough.c
@@ -5,12 +5,20 @@ int main(void){
     int age;
 
     printf("how old are you: \n");
-    scanf("%d", age);
-    if(age <= 4){
-        printf("You should not start school yet \n");
-    }else if(age == 6){
+    scanf("%d", &age);
+    if(age <= 5){
+        printf("Go play in the dirt little child dork \n");
+    }else if(age <= 7){
         printf("You should be in school\n");
+    }else if(age <= 16){
+        printf("You should get a learners permit\n");
+    }else if(age <= 17){
+        printf("You should be able to drive\n");
+    }else if(age <= 19){
+        printf("You should be able to vote\n");
+    }else{
+        printf("I'm sorry, but I don't think you exist yet\n");
     }
-    return 0;
 
+    return 0;
 }
