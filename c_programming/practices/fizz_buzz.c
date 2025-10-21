5f64c4c9e392b857a27606802819cff9838bc830
DragonUniverse1 <alexa.stephens@ucas-edu.net>
2025-10-21 14:55:02 +0000

    undone fizzbuzz

--- c_programming/practices/fizz_buzz.c
+++ c_programming/practices/fizz_buzz.c
@@ -2,10 +2,17 @@
 #include <stdio.h>
 
 int main(void){
-
     int num = 1;
     while(num < 51){
-        printf("%d\n", num);
+        if(num % 3 == 0 && num % 5 == 0){
+            printf("FizzBuzz\n");
+        }else if(num % 3 == 0){
+            printf("Fizz\n");
+        }else if (num % 5 == 0){
+        printf("Buzz\n");
+        }else{
+            print("%d\n", num);
+        }
         num++;
     }
 
